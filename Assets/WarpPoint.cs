4aec49f64180c8270f5dfc62f9f463ad45bf7476
shawmakesmusic <shawmakesmusic@gmail.com>
2019-08-19 04:15:58 -0700

    Added some artwork and level design

---
+++ Assets/WarpPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPoint : MonoBehaviour
+{
+    public uint teamIndex;
+    public uint groupNumber;
+
+    void Awake()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+}
