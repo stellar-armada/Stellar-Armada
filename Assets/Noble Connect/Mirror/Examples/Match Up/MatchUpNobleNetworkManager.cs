c732c57a361b6d501b2d3c958624004362d4fd61
Shaw <shawmakesmusic@gmail.com>
2019-12-02 17:32:04 -0500

    Update MatchUpNobleNetworkManager.cs

--- Assets/Noble Connect/Mirror/Examples/Match Up/MatchUpNobleNetworkManager.cs
+++ Assets/Noble Connect/Mirror/Examples/Match Up/MatchUpNobleNetworkManager.cs
@@ -18,6 +18,7 @@ using Mirror;
             Debug.Log("Creating match manager");
             GameObject matchManager = Instantiate(matchManagerPrefab);
             NetworkServer.Spawn(matchManager);
+            matchManager.GetComponent<NetworkIdentity>().AssignClientAuthority(NetworkServer.localConnection);
         }
 
         private void OnServerInitialized()
