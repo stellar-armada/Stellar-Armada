57e69c89b942d630854c933815c7f98fdb67fe43
Shaw <shawmakesmusic@gmail.com>
2019-12-02 20:27:19 -0500

    Update TeamManager.cs

--- Assets/Scripts/Teams/TeamManager.cs
+++ Assets/Scripts/Teams/TeamManager.cs
@@ -69,6 +69,10 @@ namespace StellarArmada.Teams
                     }
             
             NetworkServer.Spawn(t.gameObject);
+            
+            // TO-DO -- Don't give all players client authority over all teams, but w/e
+            foreach (var conn in NetworkServer.connections.Values)
+                t.GetComponent<NetworkIdentity>().AssignClientAuthority(conn);
         }
 
 
